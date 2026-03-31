bfcbff9a3f2af75ac474a28d4e225629de921f27
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-03-31 13:37:43 -0500

    Add: pascals-triangle - submission-2

---
+++ Data Structures & Algorithms/pascals-triangle/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        res.get(0).add(1);
+
+        for(int i = 1; i < numRows; i++) {
+            List<Integer> temp = new ArrayList<>(res.get(i-1));
+            temp.add(0, 0);
+            temp.add(0);
+            temp.stream().forEach(System.out::println);
+            List<Integer> row = new ArrayList<>();
+
+            for(int j = 1; j < temp.size(); j++) {
+                row.add(temp.get(j-1) + temp.get(j));
+            }
+            res.add(row);
+        }
+
+        return res;
+    }
+}

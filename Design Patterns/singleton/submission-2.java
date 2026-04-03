beca9fcbe3f947a2b42cf36067b6d303b2729325
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-04-02 20:16:45 -0500

    Add: singleton - submission-2

---
+++ Design Patterns/singleton/submission-2.java
@@ -0,0 +1,25 @@
+static class Singleton {
+    private static String value;
+    private static Singleton instance;
+
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+}

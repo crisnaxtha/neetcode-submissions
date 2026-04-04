3d892d646cdabf22b586b5f3d2070196d891297a
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-04-04 16:09:55 -0500

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> smap = new HashMap<>();
+        Map<Character, Integer> tmap = new HashMap<>();
+
+        for(char c : s.toCharArray()) { 
+            if(Character.isLetterOrDigit(c)) {
+                smap.put(c, smap.getOrDefault(c, 0) + 1);
+            }
+            
+        }
+
+        for(char c : t.toCharArray()) {
+            if(Character.isLetterOrDigit(c)) {
+                tmap.put(c, tmap.getOrDefault(c, 0) + 1);
+            }
+            
+        }
+
+        for(Map.Entry<Character, Integer> entry : smap.entrySet()) {
+            if(!entry.getValue().equals(tmap.get(entry.getKey()))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

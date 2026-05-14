4fc52c8a84fd823b86148d37db4891e9bd447e54
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-05-14 18:07:57 -0500

    Add: string-compression - submission-3

---
+++ Data Structures & Algorithms/string-compression/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compress(char[] chars) {
+        if(chars.length == 1) {
+            return 1;
+        }
+        int l = 0, r = 0;
+        int count = 0;
+        while(r < chars.length) {
+            char ch = chars[r];
+            count = 0;
+            while(r < chars.length && ch == chars[r]) {
+                count++;
+                r++;
+            }
+            chars[l] = ch;
+            l++;
+            if(count > 1) {
+                for(char c : String.valueOf(count).toCharArray()) {
+                    chars[l] = c;
+                    l++;
+                }
+            }
+        }
+        return l;
+    }
+}

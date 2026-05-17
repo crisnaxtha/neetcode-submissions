bd08378e8e265466b541e32906ef02438d73cd79
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-05-17 08:42:16 -0500

    Add: find-smallest-common-element-in-all-rows - submission-1

---
+++ Data Structures & Algorithms/find-smallest-common-element-in-all-rows/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int smallestCommonElement(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for (int c = 0; c < n; c++) {
+            int num = mat[0][c];
+            boolean foundInAll = true;
+            for (int r = 1; r < m; r++) {
+                int index = Arrays.binarySearch(mat[r], num);
+                System.out.println("r " + r + " num " + num + " index " + index);
+                if (index < 0) {
+                    foundInAll = false;
+                    break;
+                }
+            }
+            if (foundInAll) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
